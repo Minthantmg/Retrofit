3608d31776b9cabaa6c9e70eedb4a8f529565d6b
lenovo <minthant180@gmail.com>
2021-11-24 01:49:06 +0630

    display results with recyclerView

--- app/src/main/java/com/example/retrofit/MainActivity.kt
+++ app/src/main/java/com/example/retrofit/MainActivity.kt
@@ -3,40 +3,41 @@ package com.example.retrofit
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.retrofit.adapter.MyAdapter
 import com.example.retrofit.repository.Repository
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var viewModel: MainViewModel
+    private val adapter by lazy {
+        MyAdapter()
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        setUpRecyclerView()
         val repository = Repository()
         val viewModelFactory = MainViewModelFactory(repository)
         viewModel = ViewModelProvider(this,viewModelFactory).get(MainViewModel::class.java)
 
-
-        button.setOnClickListener {
-            val myNum = number_editText.text.toString()
-            viewModel.getCustomPost(Integer.parseInt(myNum),"id","desc")
-            viewModel.myCustomPost.observe(this, Observer { response ->
+        viewModel.getCustomPost(2,"id","desc")
+        viewModel.myCustomPost.observe(this, Observer { response ->
                 if (response.isSuccessful){
-                    textView.text = response.body()?.toString()
-                    response.body()?.forEach {
-                        Log.d("Responese",it.userId.toString())
-                        Log.d("Responese",it.id.toString())
-                        Log.d("Responese",it.title)
-                        Log.d("Responese",it.body)
-                        Log.d("Responese","-------------")
-                    }
+                    response.body()?.let { adapter.setData(it) }
                 }else{
-                    textView.text = response.code().toString()
+                    Toast.makeText(this,response.code().toString(),Toast.LENGTH_LONG).show()
                 }
-            })
-        }
+        })
+    }
+
+    private fun setUpRecyclerView(){
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(this)
     }
 }
